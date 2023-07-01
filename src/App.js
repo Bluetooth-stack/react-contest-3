ffad7748a6ed63a064a564f3c35350926fe1b2e6
Siddharth Hota <siddharthhota26@gmail.com>
2023-07-01 19:31:02 +0530

    components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
+import Home from "./components/Home";
+import { Routes, Route } from "react-router-dom";
+import Cart from "./components/Cart";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello</h1>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+     
     </div>
   );
 }
